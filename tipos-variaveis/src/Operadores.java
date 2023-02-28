663dfd94808564cdb423b23e39057d02c62e1a5c
Leila Vasconcelos <leila.vasconcelos.022@gmail.com>
2023-02-27 22:41:04 -0300

    Exemplo de operadores java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+   
+    public static void main(String [] args) {
+    int a, b;
+    a = 5;
+    b = 6;
+
+//Ambas dão o mesmo resultado!
+
+    String resultado = "";
+    if (a==b)
+        resultado = "verdadeiro";
+    else 
+        resultado = "falso";
+    
+    System.out.println(resultado);
+
+//---------
+
+    /*String resultado = a==b ? "verdadeiro" : "falso";
+
+    System.out.println(resultado);*/
+
+    }
+}
+
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        String nomeUm = "Leila";
+        String nomeDois = new String ("Leila");
+    
+        System.out.println(nomeUm.equals(nomeDois));
+    
+        }
 }
+
+//equals é para a comparação de igualdade, se for para 
+//ser diferente, deve-se colocar o (!x.equals(xx));
